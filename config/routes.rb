73017a4b237dd44b3ed614db546aa9b4f00f0bcd
Chris Oliver <excid3@gmail.com>
2018-08-17 23:18:52 -0500

    Add Webpacker and Tailwind

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
+# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  mount Jumpstart::Engine, at: "/jumpstart"
+
+  scope controller: :static do
+    get :about
+    get :terms
+    get :privacy
+  end
+
+  root to: "static#index"
 end
