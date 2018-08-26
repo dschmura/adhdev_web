b7095ee11a7099d898a1f30adee8643c8b5c9991
Chris Oliver <excid3@gmail.com>
2018-08-26 16:06:01 -0500

    Add sidekiq link in admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
+require 'sidekiq/web'
 # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
 Rails.application.routes.draw do
@@ -6,6 +7,10 @@ Rails.application.routes.draw do
 
   # Administrate
   namespace :admin do
+    authenticate :user, lambda { |u| u.admin? } do
+      mount Sidekiq::Web => '/sidekiq'
+    end
+
     resources :users
 
     root to: "users#index"
