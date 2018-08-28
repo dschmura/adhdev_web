cea8de751f0f002a10f1a863e3edbcedd215dc1e
Jason Charnes <jason@thecharnes.com>
2018-08-28 08:44:14 -0500

    Don't try avatar without current user in navbar

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_20_210659) do
+ActiveRecord::Schema.define(version: 2018_08_21_214212) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -61,6 +61,19 @@ ActiveRecord::Schema.define(version: 2018_08_20_210659) do
     t.datetime "updated_at"
   end
 
+  create_table "user_connected_accounts", force: :cascade do |t|
+    t.integer "user_id"
+    t.string "provider"
+    t.string "access_token"
+    t.string "refresh_token"
+    t.datetime "expires_at"
+    t.string "auth"
+    t.string "uid"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_user_connected_accounts_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -71,10 +84,8 @@ ActiveRecord::Schema.define(version: 2018_08_20_210659) do
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
     t.string "unconfirmed_email"
-    t.datetime "accepted_terms_at"
-    t.datetime "accepted_privacy_at"
-    t.string "time_zone"
     t.boolean "admin"
+    t.string "time_zone"
     t.string "provider"
     t.string "uid"
     t.datetime "created_at", null: false
