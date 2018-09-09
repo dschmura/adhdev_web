2d662e68f674d7c64ec58923de13d727d3c7b5c5
Chris Oliver <excid3@gmail.com>
2018-09-08 22:43:08 -0500

    Include comment for provider callback

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,2 +1,15 @@
 class Users::OmniauthCallbacksController < Jumpstart::OmniauthCallbacksController
+
+  # Do any extra processing on the account that was just connected
+  # The full oauth data is stored in the `auth` attribute on the
+  # ConnectedAccount model.
+  #
+  # def github_connected(connected_account)
+  # end
+  #
+  # def twitter_connected(connected_account)
+  # end
+  #
+  # etc...
+
 end
