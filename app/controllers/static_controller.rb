3b044208ac2e5d176e13a4d16fbc4760ee7f413a
Chris Oliver <excid3@gmail.com>
2018-10-05 15:51:32 -0500

    Redirect from pricing page if no plans

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -5,6 +5,10 @@ class StaticController < ApplicationController
   def about
   end
 
+  def pricing
+    redirect_to root_path, alert: "There are no pricing plans configured." if Jumpstart.config.plans.none?
+  end
+
   def terms
   end
 
