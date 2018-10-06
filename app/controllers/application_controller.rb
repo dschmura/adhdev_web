59649c0c3773368a551c887ae10e3babd6b515e1
Chris Oliver <excid3@gmail.com>
2018-10-06 12:44:50 -0500

    Include jumpstart controller in application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,6 @@
 class ApplicationController < ActionController::Base
+  include Jumpstart::Controller
+
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :masquerade_user!
 
