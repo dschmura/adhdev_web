29c25f1d8f8d7d46c0f1d7728f6640547e467112
Chris Oliver <excid3@gmail.com>
2018-12-18 21:17:02 -0600

    Use tailwind stimulus components instead

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -1,4 +1,4 @@
-// Load all the controllers within this directory and all subdirectories. 
+// Load all the controllers within this directory and all subdirectories.
 // Controller files must be named *_controller.js.
 
 import { Application } from "stimulus"
@@ -7,3 +7,8 @@ import { definitionsFromContext } from "stimulus/webpack-helpers"
 const application = Application.start()
 const context = require.context("controllers", true, /_controller\.js$/)
 application.load(definitionsFromContext(context))
+
+import { Dropdown, Modal, Tabs } from "tailwindcss-stimulus-components"
+application.register('dropdown', Dropdown)
+application.register('modal', Modal)
+application.register('tabs', Tabs)
