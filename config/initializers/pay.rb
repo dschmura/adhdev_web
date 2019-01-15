679c4465dd4ae49aae8804e50333cb5b91b98085
Chris Oliver <excid3@gmail.com>
2019-01-14 19:55:34 -0600

    Add stripe event

--- config/initializers/pay.rb
+++ config/initializers/pay.rb
@@ -1,14 +1,13 @@
 Pay.setup do |config|
-  credentials = Rails.application.credentials
-  env         = Rails.env.to_sym
-  merchant_id = credentials.dig(env, :braintree, :merchant_key)
+  credentials = Rails.application.credentials.dig(Rails.env.to_sym, :braintree)
+  merchant_id = credentials.dig(:merchant_key)
 
   if merchant_id.present?
     config.braintree_gateway = ::Braintree::Gateway.new(
-      environment: credentials.dig(env, :braintree, :environment).to_sym,
+      environment: credentials.dig(:environment).to_sym,
       merchant_id: merchant_id,
-      public_key:  credentials.dig(env, :braintree, :public_key),
-      private_key: credentials.dig(env, :braintree, :private_key)
+      public_key:  credentials.dig(:public_key),
+      private_key: credentials.dig(:private_key)
     )
   end
 end
