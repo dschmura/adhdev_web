44692304440c64042b8b0ab48d2738d0ef93403a
Chris Oliver <excid3@gmail.com>
2019-04-23 22:39:27 -0500

    Hide plans toggle if there are only monthly or only yearly plans

--- app/javascript/controllers/pricing_controller.js
+++ app/javascript/controllers/pricing_controller.js
@@ -1,16 +1,31 @@
 import { Controller } from "stimulus"
 
 export default class extends Controller {
-  static targets = [ "monthlyToggle", "yearlyToggle", "monthlyPlans", "yearlyPlans" ]
+  static targets = [ "toggle", "monthlyToggle", "yearlyToggle", "monthlyPlans", "yearlyPlans" ]
 
   connect() {
     this.plansClass     = (this.data.get("plansClass") || "hidden").split(" ")
     this.frequencyClass = (this.data.get("frequency-class") || "bg-white text-black").split(" ")
+
+    let monthly_plans = this.monthlyPlansTarget.children.length
+    let yearly_plans = this.yearlyPlansTarget.children.length
+
+    // Hide toggle if no plans in one section
+    if (monthly_plans > 0 && yearly_plans == 0) {
+      this._hideToggle()
+    } else if (monthly_plans == 0 && yearly_plans > 0) {
+      this._toggle("yearly")
+      this._hideToggle()
+    }
   }
 
   switch(event) {
     event.preventDefault()
     this._toggle(event.target.dataset.show)
+  }
+
+  _hideToggle() {
+    this.toggleTarget.classList.add("hidden")
   }
 
   _toggle(frequency) {
