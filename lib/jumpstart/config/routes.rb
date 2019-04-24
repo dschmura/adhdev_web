54af2b88e2097c8e3418e12f3315c34bf3712576
Chris Oliver <excid3@gmail.com>
2019-04-23 23:10:56 -0500

    Add branding docs

--- lib/jumpstart/config/routes.rb
+++ lib/jumpstart/config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
 
     resource :docs do
       [:announcements, :billing, :scaffolds, :teams, :users].each{ |doc| get doc }
-      [:alerts, :buttons, :cards, :forms, :icons, :javascript, :pagination, :pills, :typography, :wells].each{ |doc| get doc }
+      [:alerts, :branding, :buttons, :cards, :forms, :icons, :javascript, :pagination, :pills, :typography, :wells].each{ |doc| get doc }
     end
 
     root to: "admin#show"
