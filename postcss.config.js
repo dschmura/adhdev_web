1f80c4ced0cb6315eaf87fe957efa39b5c1aa173
Chris Oliver <excid3@gmail.com>
2019-04-24 19:07:23 -0500

    Upgrade to tailwindcss 1.0 beta 5 and fix tailwind config file location

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,6 @@
 let environment = {
   plugins: [
-    require('tailwindcss'),
+    require('tailwindcss')('./app/javascript/stylesheets/tailwind.config.js'),
     require('autoprefixer'),
     require('postcss-import'),
     require('postcss-flexbugs-fixes'),
