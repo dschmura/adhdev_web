0f60ce9d7a022cec14b28bcff5497c097a596dcc
Chris Oliver <excid3@gmail.com>
2019-05-30 22:23:10 -0500

    Fix delayed job queue adapter

--- lib/jumpstart/lib/jumpstart/job_processor.rb
+++ lib/jumpstart/lib/jumpstart/job_processor.rb
@@ -3,7 +3,7 @@ module Jumpstart
     AVAILABLE_PROVIDERS = {
       "Async"       => :async,
       "Sidekiq"     => :sidekiq,
-      "DelayedJob"  => :delayed_job_active_record,
+      "DelayedJob"  => :delayed_job,
       "Sneakers"    => :sneakers,
       "SuckerPunch" => :sucker_punch,
     }.freeze
