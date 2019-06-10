d5bc14d69f8efcfbdc34e626bf5c6f29d2735580
Chris Oliver <excid3@gmail.com>
2019-06-10 17:05:11 -0500

    Refactor scaffold generator overrides

---
+++ lib/jumpstart/lib/generators/rails/scaffold_controller_generator.rb
@@ -0,0 +1,26 @@
+require 'rails/generators'
+require 'rails/generators/erb/scaffold/scaffold_generator'
+
+module Erb
+  module Generators
+    class ScaffoldGenerator
+      source_paths << File.expand_path('../templates', __FILE__)
+
+      def copy_view_files
+        available_views.each do |view|
+          formats.each do |format|
+            filename = filename_with_extensions(view, format)
+            template filename, File.join("app/views", controller_file_path, filename)
+          end
+        end
+      end
+
+      def add_to_navigation
+        append_to_file "app/views/shared/_left_nav.html.erb" do
+          "<%= nav_link_to \"#{plural_name.titleize}\", #{name.tableize}_path, class: 'nav-link' %>\n"
+        end
+      end
+
+    end
+  end
+end
