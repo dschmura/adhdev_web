4c7242d6ff5fcb1d8bd3a81198a538a2df449480
Chris Oliver <excid3@gmail.com>
2019-06-14 11:40:44 -0500

    Fix table name in migration

--- db/migrate/20180821214212_create_user_connected_accounts.rb
+++ db/migrate/20180821214212_create_user_connected_accounts.rb
@@ -1,6 +1,6 @@
 class CreateUserConnectedAccounts < ActiveRecord::Migration[5.2]
   def change
-    create_table :connected_accounts do |t|
+    create_table :users_connected_accounts do |t|
       t.references :user, foreign_key: true
       t.string :provider
       t.string :uid
