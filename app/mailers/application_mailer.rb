70a2bebc0c041087ec9e91073db235ddc87e7678
Chris Oliver <excid3@gmail.com>
2019-06-24 18:41:38 -0500

    Add ApplicationHelper to mailers by default

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,3 +1,6 @@
 class ApplicationMailer < ActionMailer::Base
   layout 'mailer'
+
+  # Include any view helpers from your main app to use in mailers here
+  add_template_helper(ApplicationHelper)
 end
