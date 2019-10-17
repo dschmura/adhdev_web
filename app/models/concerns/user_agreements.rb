de01bba808d894a117228a4d6448e0c3f118fc2a
Chris Oliver <excid3@gmail.com>
2019-10-16 21:14:33 -0500

    Validate presence of terms of service

--- app/models/concerns/user_agreements.rb
+++ app/models/concerns/user_agreements.rb
@@ -4,7 +4,7 @@ module UserAgreements
   included do
     # Accept the terms of service on registration
     attribute :terms_of_service
-    validates :terms_of_service, acceptance: true, on: [:create, :invitation_accepted]
+    validates :terms_of_service, presence: true, acceptance: true, on: [:create, :invitation_accepted]
 
     after_validation :accept_terms, on: [:create, :invitation_accepted]
     after_validation :accept_privacy, on: [:create, :invitation_accepted]
