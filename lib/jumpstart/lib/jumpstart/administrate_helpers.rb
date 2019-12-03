adc35e3eb64476bbda8812610c61eb4ff2038736
Chris Oliver <excid3@gmail.com>
2019-12-02 21:57:37 -0600

    Link to Stripe test url if public key starts with pk_test. Closes #19

--- lib/jumpstart/lib/jumpstart/administrate_helpers.rb
+++ lib/jumpstart/lib/jumpstart/administrate_helpers.rb
@@ -4,44 +4,45 @@ module Jumpstart
     def charge_processor_url(charge)
       case charge.processor
       when "stripe"
-        "https://dashboard.stripe.com/payments/#{charge.processor_id}"
-
+        "#{stripe_base_url}/payments/#{charge.processor_id}"
       when "braintree"
-        config      = Pay.braintree_gateway.config
-        merchant_id = config.merchant_id
-        environment = (config.environment.to_s == "sandbox" ? "sandbox" : "www")
-
-        "https://#{environment}.braintreegateway.com/merchants/#{merchant_id}/transactions/#{charge.processor_id}"
+        "#{braintree_base_url}/transactions/#{charge.processor_id}"
       end
     end
 
     def customer_processor_url(user)
       case user.processor
       when "stripe"
-        "https://dashboard.stripe.com/customers/#{user.processor_id}"
-
+        "#{stripe_base_url}/customers/#{user.processor_id}"
       when "braintree"
-        config      = Pay.braintree_gateway.config
-        merchant_id = config.merchant_id
-        environment = (config.environment.to_s == "sandbox" ? "sandbox" : "www")
-
-        "https://#{environment}.braintreegateway.com/merchants/#{merchant_id}/customers/#{user.processor_id}"
+        "#{braintree_base_url}/customers/#{user.processor_id}"
       end
     end
 
     def subscription_processor_url(subscription)
       case subscription.processor
       when "stripe"
-        "https://dashboard.stripe.com/subscriptions/#{subscription.processor_id}"
-
+        "#{stripe_base_url}/subscriptions/#{subscription.processor_id}"
       when "braintree"
+        "#{braintree_base_url}/subscriptions/#{subscription.processor_id}"
+      end
+    end
+
+    private
+
+      def stripe_base_url
+        url = "https://dashboard.stripe.com"
+        url += "/test" if Pay::Stripe.public_key.start_with?("pk_test")
+        byebug
+        url
+      end
+
+      def braintree_base_url
         config      = Pay.braintree_gateway.config
         merchant_id = config.merchant_id
         environment = (config.environment.to_s == "sandbox" ? "sandbox" : "www")
 
-        "https://#{environment}.braintreegateway.com/merchants/#{merchant_id}/subscriptions/#{subscription.processor_id}"
+        "https://#{environment}.braintreegateway.com/merchants/#{merchant_id}"
       end
-    end
-
   end
 end
