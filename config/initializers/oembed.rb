9d6120e092ed32ec627d5f201c78568bdcb4b988
Chris Oliver <excid3@gmail.com>
2020-01-19 13:44:39 -0600

    Add OEmbed fallbacks

--- config/initializers/oembed.rb
+++ config/initializers/oembed.rb
@@ -1,4 +1,8 @@
 OEmbed::Providers.register_all
+OEmbed::Providers.register_fallback(
+  OEmbed::ProviderDiscovery,
+  OEmbed::Providers::Noembed
+)
 
 Rails.application.config.to_prepare do
   ActionText::ContentHelper.allowed_tags += %w(iframe script blockquote time)
