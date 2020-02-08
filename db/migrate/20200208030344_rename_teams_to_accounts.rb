ba8b1dff7bcd26a72636953b13b40f539e3a60d7
Chris Oliver <excid3@gmail.com>
2020-02-08 15:19:47 -0600

    Rename team_id columns first

--- db/migrate/20200208030344_rename_teams_to_accounts.rb
+++ db/migrate/20200208030344_rename_teams_to_accounts.rb
@@ -2,11 +2,14 @@ class RenameTeamsToAccounts < ActiveRecord::Migration[6.0]
   def change
     if table_exists? :teams
       safety_assured do
+        rename_column :team_members, :team_id, :account_id
+        rename_column :team_invitations, :team_id, :account_id
+
+        # TODO: Rename any team_id columns for your models here
+
         rename_table :teams, :accounts
         rename_table :team_members, :account_users
         rename_table :team_invitations, :account_invitations
-        rename_column :team_members, :team_id, :account_id
-        rename_column :team_invitations, :team_id, :account_id
       end
     end
   end
