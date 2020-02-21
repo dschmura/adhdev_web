7615ac63fe64d880950061b1f22fb55530a16b8b
Chris Oliver <excid3@gmail.com>
2020-02-21 12:38:50 -0600

    Set time zone for the request if user signed in

--- app/controllers/concerns/users/time_zone.rb
+++ app/controllers/concerns/users/time_zone.rb
@@ -4,6 +4,7 @@ module Users
 
     included do
       helper_method :browser_time_zone
+      around_action :set_time_zone
     end
 
     def browser_time_zone
@@ -11,6 +12,14 @@ module Users
       ActiveSupport::TimeZone.all.find { |zone| zone.tzinfo == browser_tz } || Time.zone
     rescue TZInfo::UnknownTimezone, TZInfo::InvalidTimezoneIdentifier
       Time.zone
+    end
+
+    def set_time_zone
+      if user_signed_in? && current_user.time_zone?
+        Time.use_zone(current_user.time_zone) { yield }
+      else
+        yield
+      end
     end
   end
 end
