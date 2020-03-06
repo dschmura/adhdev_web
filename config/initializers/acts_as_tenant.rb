01e8683c203ba889953c88c95c13303bb78e696a
Chris Oliver <excid3@gmail.com>
2020-03-05 18:10:28 -0600

    Add docs for Accounts and Multitenancy

---
+++ config/initializers/acts_as_tenant.rb
@@ -0,0 +1,6 @@
+# ActsAsTenant configuration - https://github.com/ErwinM/acts_as_tenant
+# Use this to configure how multitenancy works if you're using it.
+
+ActsAsTenant.configure do |config|
+  config.require_tenant = false
+end
