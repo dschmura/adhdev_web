c94ca0f51c8cd99e290db6e2ac7427ca20c25422
Chris Oliver <excid3@gmail.com>
2020-03-11 21:17:08 -0500

    Fix passwords controller

--- app/controllers/account/passwords_controller.rb
+++ app/controllers/account/passwords_controller.rb
@@ -1,27 +1,25 @@
-module Account
-  class PasswordsController < ApplicationController
-    before_action :authenticate_user!
+class Account::PasswordsController < ApplicationController
+  before_action :authenticate_user!
 
-    def show
-      redirect_to action: :edit
-    end
-
-    def edit
-    end
-
-    def update
-      if current_user.update_with_password(password_params)
-        bypass_sign_in current_user
-        redirect_to account_password_path, notice: "Your password was changed successfully"
-      else
-        render action: :edit
-      end
-    end
-
-    private
-
-      def password_params
-        params.require(:user).permit(:current_password, :password, :password_confirmation)
-      end
+  def show
+    redirect_to action: :edit
   end
+
+  def edit
+  end
+
+  def update
+    if current_user.update_with_password(password_params)
+      bypass_sign_in current_user
+      redirect_to account_password_path, notice: "Your password was changed successfully"
+    else
+      render action: :edit
+    end
+  end
+
+  private
+
+    def password_params
+      params.require(:user).permit(:current_password, :password, :password_confirmation)
+    end
 end
