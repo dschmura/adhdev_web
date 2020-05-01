1c435dcf215eca5b180cc3dfaf28bedcdb9c5c82
Chris Oliver <excid3@gmail.com>
2020-05-01 11:14:13 -0500

    Whitelist flatpickr in purgecss

--- postcss.config.js
+++ postcss.config.js
@@ -29,7 +29,7 @@ if (process.env.RAILS_ENV === "production" || process.env.RAILS_ENV === "staging
       defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || [],
       whitelist: collectWhitelist(),
       whitelistPatterns: [],
-      whitelistPatternsChildren: [/trix/, /attachment/, /tribute/, /tippy/],
+      whitelistPatternsChildren: [/trix/, /attachment/, /tribute/, /tippy/, /flatpickr/],
     })
   )
 }
