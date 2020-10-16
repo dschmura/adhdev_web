453ac800feca6c883f98294ae4fa1ba79dcf19f7
Chris Oliver <excid3@gmail.com>
2020-10-15 19:14:28 -0500

    add_template_helper was removed in Rails 6.1

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -3,5 +3,5 @@ class ApplicationMailer < ActionMailer::Base
   layout "mailer"
 
   # Include any view helpers from your main app to use in mailers here
-  add_template_helper(ApplicationHelper)
+  helper ApplicationHelper
 end
