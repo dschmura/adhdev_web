6e31458d6ae78cd1cc3918de6a204864b34af38f
Vlad Gorodetsky <v@gor.io>
2021-01-02 13:19:06 +0200

    Fix config after sentry sdk upgrade

--- lib/jumpstart/lib/templates/config/initializers/sentry.rb
+++ lib/jumpstart/lib/templates/config/initializers/sentry.rb
@@ -1,3 +1,6 @@
-Raven.configure do |config|
+Sentry.init do |config|
   config.dsn = Rails.application.credentials.sentry[:dsn]
+  config.send_default_pii = true
+  config.rails.report_rescued_exceptions = true
+  config.breadcrumbs_logger = [:active_support_logger]
 end
