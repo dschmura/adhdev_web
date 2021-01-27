820e66182fab8aeef909a5ca12093922d9ea1410
Chris Oliver <excid3@gmail.com>
2021-01-27 10:27:03 -0600

    Standardize

--- test/system/login_test.rb
+++ test/system/login_test.rb
@@ -2,13 +2,13 @@ require "application_system_test_case"
 
 class LoginTest < ApplicationSystemTestCase
   test "can login" do
-    Capybara.app_host = 'http://lvh.me'
+    Capybara.app_host = "http://lvh.me"
     Capybara.always_include_port = true
 
     visit new_user_session_path
 
-    fill_in 'user[email]', with: users(:one).email
-    fill_in 'user[password]', with: 'password'
+    fill_in "user[email]", with: users(:one).email
+    fill_in "user[password]", with: "password"
 
     find('input[name="commit"]').click
 
