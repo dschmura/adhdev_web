421ee77320a7412e074959c2b1043a370259081c
Chris Oliver <excid3@gmail.com>
2021-02-03 08:33:43 -0600

    Update comment for jsonb

--- app/models/account_user.rb
+++ app/models/account_user.rb
@@ -42,14 +42,11 @@ class AccountUser < ApplicationRecord
     define_method(:"#{role}?") { send(role) }
   end
 
-  # You can use Postgres' array operators to query the roles array column
-  # https://www.postgresql.org/docs/current/functions-array.html
-  #
-  # Query where arrays overlap:
-  # scope :managers, -> { where("roles && ?", "{manager, supervisor}") }
+  # You can use Postgres' jsonb operators to query the roles jsonb column
+  # https://www.postgresql.org/docs/current/functions-json.html
   #
   # Query where roles contains:
-  # scope :managers, -> { where("roles @> ?", "{manager}") }
+  # scope :managers, -> { where("roles @> ?", {manager: true}.to_json) }
 
   def active_roles
     ROLES.select { |role| send(:"#{role}?") }.compact
