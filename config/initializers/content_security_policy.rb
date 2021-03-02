97b5d8a376b1ded13598462f66105001c2b9d265
Chris Oliver <excid3@gmail.com>
2021-03-02 17:10:03 -0600

    Clean up CSP config

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -5,55 +5,32 @@
 # https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Content-Security-Policy
 
 Rails.application.config.content_security_policy do |policy|
-  #   policy.default_src :self, :https
-  #   policy.font_src    :self, :https, :data
-  #   policy.img_src     :self, :https, :data
-  #   policy.object_src  :none
-  #   policy.script_src  :self, :https
-  #   policy.style_src   :self, :https
+#   policy.default_src :self, :https
+#   policy.font_src    :self, :https, :data
+#   policy.img_src     :self, :https, :data
+#   policy.object_src  :none
+#   policy.script_src  :self, :https
+#   policy.style_src   :self, :https
+
   # If you are using webpack-dev-server then specify webpack-dev-server host
-  # If you are using Rack::LiveReload, add "http://localhost:35729" and "ws://localhost:35729", respectively
   if Rails.env.development?
     srcs = ["http://localhost:3035", "ws://localhost:3035", "ws://localhost:3000", "ws://localhost:5000"]
     srcs += ["http://localhost:35729", "ws://localhost:35729"] if Jumpstart.config.livereload?
     policy.connect_src :self, :https, *srcs
   end
 
-  #   # Specify URI for violation reports
-  #   # policy.report_uri "/csp-violation-report-# Be sure to restart your server when you modify this file.
+  # For subdomains, add your domain:
+  # policy.connect_src :self, :https, "*.domain.com"
 
-  # Define an application-wide content security policy
-  # For further information see the following documentation
-  # https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Content-Security-Policy
-
-  # Rails.application.config.content_security_policy do |policy|
-  #   policy.default_src :self, :https
-  #   policy.font_src    :self, :https, :data
-  #   policy.img_src     :self, :https, :data
-  #   policy.object_src  :none
-  #   policy.script_src  :self, :https
-  #   policy.style_src   :self, :https
-  #   # If you are using webpack-dev-server then specify webpack-dev-server host
-  #   policy.connect_src :self, :https, "http://localhost:3035", "ws://localhost:3035" if Rails.env.development?
-
-  #   # Specify URI for violation reports
-  #   # policy.report_uri "/csp-violation-report-endpoint"
-  # end
-
-  # If you are using UJS then enable automatic nonce generation
-  # Rails.application.config.content_security_policy_nonce_generator = -> request { SecureRandom.base64(16) }
-
-  # Set the nonce only to specific directives
-  # Rails.application.config.content_security_policy_nonce_directives = %w(script-src)
-
-  # Report CSP violations to a specified URI
-  # For further information see the following documentation:
-  # https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Content-Security-Policy-Report-Only
-  # Rails.application.config.content_security_policy_report_only = trueendpoint"
+#   # Specify URI for violation reports
+#   # policy.report_uri "/csp-violation-report-endpoint"
 end
 
 # If you are using UJS then enable automatic nonce generation
 # Rails.application.config.content_security_policy_nonce_generator = -> request { SecureRandom.base64(16) }
+
+# Set the nonce only to specific directives
+# Rails.application.config.content_security_policy_nonce_directives = %w(script-src)
 
 # Report CSP violations to a specified URI
 # For further information see the following documentation:
