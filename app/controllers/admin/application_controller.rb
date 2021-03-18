99521f6034124c1e6ec441f3fc0a6f19571e6f3a
Chris Oliver <excid3@gmail.com>
2021-03-18 09:44:43 -0500

    Fix test

--- app/controllers/admin/application_controller.rb
+++ app/controllers/admin/application_controller.rb
@@ -7,7 +7,7 @@
 module Admin
   class ApplicationController < Administrate::ApplicationController
     before_action :authenticate_admin
-    around_action :without_tennant
+    around_action :without_tenant
 
     helper all_helpers_from_path "app/helpers"
 
@@ -30,12 +30,12 @@ module Admin
     # def records_per_page
     #   params[:per_page] || 20
     # end
-  end
 
-  # Allow the admin area to view all records
-  def without_tennant
-    ActsAsTenant.without_tenant do
-      yield
+    # Allow the admin area to view all records
+    def without_tenant
+      ActsAsTenant.without_tenant do
+        yield
+      end
     end
   end
 end
