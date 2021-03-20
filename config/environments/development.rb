2cd9d1d9a9626f95f9262c01f61966853305c0b3
Chris Oliver <excid3@gmail.com>
2021-03-20 12:17:51 -0500

    Use localhost in default_url_options until we have the CSP configured and tested for lvh.me as well

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -75,5 +75,5 @@ Rails.application.configure do
   config.hosts = nil
 
   # ActiveStorage / ActionText need this set to include the correct URLs from Turbo, etc
-  config.action_controller.default_url_options = {host: "lvh.me", port: ENV.fetch("PORT", 3000).to_i}
+  config.action_controller.default_url_options = {host: "localhost", port: ENV.fetch("PORT", 3000).to_i}
 end
