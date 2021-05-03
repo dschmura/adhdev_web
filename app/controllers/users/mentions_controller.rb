25c9c0524fd6f3cc8492317506b0bcf28e904326
Chris Oliver <excid3@gmail.com>
2021-05-03 08:51:47 -0500

    Document user mentions scoped to current account by default

--- app/controllers/users/mentions_controller.rb
+++ app/controllers/users/mentions_controller.rb
@@ -2,10 +2,18 @@ class Users::MentionsController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @users = current_account.users.search_by_full_name(params[:query]).with_attached_avatar
+    @users = searchable_users.search_by_full_name(params[:query]).with_attached_avatar
 
     respond_to do |format|
       format.json
     end
+  end
+
+  private
+
+  # By default, we'll only show the users in the current account.
+  # You may want to use User.all instead to allow mentioning all users.
+  def searchable_users
+    current_account.users
   end
 end
