6091c20f994c5009c430aea8ab7e2d89714743ad
Chris Oliver <excid3@gmail.com>
2021-05-03 15:52:06 -0500

    Upgrade to Webpacker 6.0.0-beta.7 which fixes the CSS filename in development

--- config/webpack/base.js
+++ config/webpack/base.js
@@ -1,16 +1,8 @@
 const { webpackConfig, merge } = require('@rails/webpacker')
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-webpackConfig.plugins = webpackConfig.plugins.filter(p => p.constructor.name != 'MiniCssExtractPlugin')
-
 const customConfig = {
   // Use cheap sourcemap generation
   devtool: 'eval-cheap-module-source-map',
-  plugins: [
-    new MiniCssExtractPlugin({
-      filename: 'css/[name]-[contenthash:8].css',
-      chunkFilename: 'css/[id]-[contenthash:8].css'
-    })
-  ],
+  plugins: [],
   resolve: {
     extensions: ['.css']
   }
