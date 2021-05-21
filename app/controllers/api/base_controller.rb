3193df1443ee053b02e2f8929a92f7a85f38bdb4
Dr Nic Williams <drnicwilliams@gmail.com>
2021-05-21 11:57:04 +1000

    Login via api token before calling #set_request_details
    API controllers now have Current.account provided

--- app/controllers/api/base_controller.rb
+++ app/controllers/api/base_controller.rb
@@ -1,6 +1,6 @@
 class Api::BaseController < ApplicationController
   skip_before_action :verify_authenticity_token
-  before_action :authenticate_api_token!
+  prepend_before_action :authenticate_api_token!
 
   private
 
@@ -20,6 +20,8 @@ class Api::BaseController < ApplicationController
     @_api_token ||= ApiToken.find_by(token: token_from_header)
   end
 
+  # Only for use within authenticate_api_token! above
+  # Use current_user/Current.user or current_account/Current.account within app controllers
   def user_from_token
     if api_token.present?
       api_token.touch(:last_used_at)
