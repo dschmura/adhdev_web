c901a197b2896432ef2d3759b4e9621263533bef
Chris Oliver <excid3@gmail.com>
2021-06-14 13:01:30 -0500

    Convert Administrate JS to Stimulus controllers. Fixes #3

--- app/javascript/administrate/index.js
+++ app/javascript/administrate/index.js
@@ -1,3 +1,8 @@
-import './components/select'
-import './components/table'
-import './components/date_time_picker'
+import { Application } from "stimulus"
+import { definitionsFromContext } from "stimulus/webpack-helpers"
+
+const application = Application.start()
+const context = require.context("./controllers", true, /_controller\.js$/)
+application.load(definitionsFromContext(context))
+
+import "./components/table"
