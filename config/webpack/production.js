b9844f707f5c0b8ae5000eb3293e6f1d5554df00
Chris Oliver <excid3@gmail.com>
2021-08-31 13:00:57 -0500

    Disable source-maps in production. Fixes #35 (#37)
    * Disable source-maps in production. Fixes #35
    
    * Clearer disable

--- config/webpack/production.js
+++ config/webpack/production.js
@@ -1,5 +1,10 @@
 process.env.NODE_ENV = process.env.NODE_ENV || 'production'
 
 const webpackConfig = require('./base')
+const { merge } = require('@rails/webpacker')
 
-module.exports = webpackConfig
+const customConfig = {
+  devtool: false
+}
+
+module.exports = merge(webpackConfig, customConfig)
