b70cdd043f3e04512b0671b35ca92b7acde87cdf
Chris Oliver <excid3@gmail.com>
2021-10-18 10:52:49 -0500

    Add viewport helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -45,4 +45,9 @@ module ApplicationHelper
   def title(page_title)
     content_for(:title) { page_title }
   end
+
+  def viewport_meta_tag(content: "width=device-width, initial-scale=1", turbo_native: "maximum-scale=1.0, user-scalable=0")
+    full_content = [content, (turbo_native if turbo_native_app?)].compact.join(", ")
+    tag.meta name: "viewport", content: full_content
+  end
 end
