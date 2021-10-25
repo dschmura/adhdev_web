a834790526a3f09156498addc5e1c908fdc80758
Chris Oliver <excid3@gmail.com>
2021-10-25 15:47:52 -0500

    Parallelize tests (#98)

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -9,6 +9,9 @@ require "minitest/mock"
 require "sidekiq/testing" if defined?(Sidekiq)
 
 class ActiveSupport::TestCase
+  # Run tests in parallel with specified workers
+  parallelize(workers: :number_of_processors)
+
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
 
