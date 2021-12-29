299e39d429758c4ffe586d322f85ad034f6fa3ad
Chris Oliver <excid3@gmail.com>
2021-12-29 09:15:58 -0600

    Use pay helper for data column

--- db/migrate/20201206085920_add_data_to_pay_models.pay.rb
+++ db/migrate/20201206085920_add_data_to_pay_models.pay.rb
@@ -1,20 +1,7 @@
 # This migration comes from pay (originally 20200603134434)
 class AddDataToPayModels < ActiveRecord::Migration[4.2]
   def change
-    add_column :pay_subscriptions, :data, data_column_type
-    add_column :pay_charges, :data, data_column_type
-  end
-
-  def data_column_type
-    config = ActiveRecord::Base.configurations.configs_for(env_name: "default", name: "primary")
-
-    case config.adapter
-    when "mysql2"
-      :json
-    when "postgresql"
-      :jsonb
-    else
-      :text
-    end
+    add_column :pay_subscriptions, :data, Pay::Adapter.json_column_type
+    add_column :pay_charges, :data, Pay::Adapter.json_column_type
   end
 end
