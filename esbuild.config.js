ef77f803efc39ae2a6678b800801c41a9f6b33b5
Chris Oliver <excid3@gmail.com>
2022-03-31 15:21:48 -0500

    Improve esbuild reloading

--- esbuild.config.js
+++ esbuild.config.js
@@ -33,39 +33,69 @@ const config = {
 }
 
 async function buildAndReload() {
-  const chokidar = require('chokidar')
-  const http = require('http')
+  const chokidar = require("chokidar")
+  const http = require("http")
 
   // Foreman & Overmind assign a separate PORT for each process
   const port = parseInt(process.env.PORT)
 
   // Reload uses an HTTP server as an even stream to reload the browser
-  http.createServer((req, res) => {
-    return clients.push(
-      res.writeHead(200, {
-        "Content-Type": "text/event-stream",
-        "Cache-Control": "no-cache",
-        "Access-Control-Allow-Origin": "*",
-        Connection: "keep-alive",
-      }),
-    );
-  }).listen(port);
+  http
+    .createServer((req, res) => {
+      return clients.push(
+        res.writeHead(200, {
+          "Content-Type": "text/event-stream",
+          "Cache-Control": "no-cache",
+          "Access-Control-Allow-Origin": "*",
+          Connection: "keep-alive",
+        })
+      )
+    })
+    .listen(port)
 
-  let result = await esbuild.build({
-    ...config,
-    incremental: true,
-    banner: {
-      js: ` (() => new EventSource("http://localhost:${port}").onmessage = () => location.reload())();`,
-    },
-  })
+  const initialize_es_build = async () => {
+    return await esbuild.build({
+      ...config,
+      incremental: true,
+      banner: {
+        js: ` (() => new EventSource("http://localhost:${port}").onmessage = () => location.reload())();`,
+      },
+    })
+  }
 
-  chokidar.watch(watchDirectories).on('all', (event, path) => {
-    if (path.includes("javascript")) {
-      result.rebuild()
-    }
-    clients.forEach((res) => res.write('data: update\n\n'))
-    clients.length = 0
-  });
+  let result = await initialize_es_build()
+  console.log("[reload] initial build succeeded")
+
+  let start = false
+  chokidar
+    .watch(watchDirectories)
+    .on("ready", () => {
+      console.log("[reload] ready")
+      start = true
+    })
+    .on("all", async (event, path) => {
+      console.log(event)
+      console.log(path)
+      if (start === false) {
+        return
+      }
+      if (path.includes("javascript")) {
+        try {
+          if (!result) {
+            console.log("[reload] reinitializing...")
+            result = await initialize_es_build()
+          }
+          await result.rebuild()
+          console.log("[reload] build succeeded")
+        } catch (error) {
+          console.error("[reload] build failed", error)
+          // clear the rebuilder so we can force it to reinitialize next time a file is changed
+          result = null
+        }
+      }
+      clients.forEach((res) => res.write("data: update\n\n"))
+      clients.length = 0
+    })
 }
 
 if (process.argv.includes("--reload")) {
@@ -75,20 +105,20 @@ if (process.argv.includes("--reload")) {
   // Watch uses esbuild's watch option
   const watch = process.argv.includes("--watch") && {
     onRebuild(error) {
-      if (error) console.error("[watch] build failed", error);
-      else console.log("[watch] build finished");
+      if (error) console.error("[watch] build failed", error)
+      else console.log("[watch] build finished")
     },
-  };
+  }
 
   esbuild.build({
     ...config,
     watch: watch,
-  }).catch(() => process.exit(1));
+  }).catch(() => process.exit(1))
 
 } else {
   // Standard esbuild
   esbuild.build({
     ...config,
     minify: process.env.RAILS_ENV == "production",
-  }).catch(() => process.exit(1));
+  }).catch(() => process.exit(1))
 }
